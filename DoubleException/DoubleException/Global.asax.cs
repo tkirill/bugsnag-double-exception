54d305d4dcffcc4a3a9b2af57b0871bae78f23ba
t_kirill <t_kirill@skbkontur.ru>
2015-08-12 18:59:04 +0500

    Create empty ASP.NET MVC 5 project

---
+++ DoubleException/DoubleException/Global.asax.cs
@@ -0,0 +1,14 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace DoubleException
+{
+    public class MvcApplication : HttpApplication
+    {
+        protected void Application_Start()
+        {
+            RouteTable.Routes.MapMvcAttributeRoutes();
+        }
+    }
+}
